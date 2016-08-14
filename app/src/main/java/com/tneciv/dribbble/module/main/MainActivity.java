39442ca73bcdba08125e70af1c2e594f5b22a176
Tneciv <tnecivl@gmail.com>
2016-08-14 16:58:44 +0800

    add dependcies
    add dependcies

--- app/src/main/java/com/tneciv/dribbble/module/main/MainActivity.java
+++ app/src/main/java/com/tneciv/dribbble/module/main/MainActivity.java
@@ -1,15 +1,28 @@
 package com.tneciv.dribbble.module.main;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.tneciv.dribbble.R;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.tv_result)
+    TextView tvResult;
+    @BindView(R.id.btn_test)
+    Button btnTest;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        btnTest.setOnClickListener(view -> Toast.makeText(this, "HH", Toast.LENGTH_SHORT).show());
     }
 }
