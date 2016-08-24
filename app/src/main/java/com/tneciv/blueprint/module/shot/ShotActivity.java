0e1e2ec68970b0dabccfd1810cc265a73f06e5c1
Tneciv <tnecivl@gmail.com>
2016-08-24 19:15:46 +0800

    edit detail layout

--- app/src/main/java/com/tneciv/blueprint/module/shot/ShotActivity.java
+++ app/src/main/java/com/tneciv/blueprint/module/shot/ShotActivity.java
@@ -1,12 +1,14 @@
 package com.tneciv.blueprint.module.shot;
 
+import android.os.Build;
 import android.os.Bundle;
+import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.text.TextUtils;
+import android.view.View;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
-import android.widget.TextView;
 
 import com.squareup.picasso.Picasso;
 import com.tneciv.blueprint.R;
@@ -20,16 +22,21 @@ public class ShotActivity extends AppCompatActivity {
 
     @BindView(R.id.imgContent)
     ImageView imgContent;
-    @BindView(R.id.custTitle)
-    TextView custTitle;
     @BindView(R.id.toolbar)
     Toolbar toolbar;
     @BindView(R.id.contentArea)
     FrameLayout contentArea;
+    @BindView(R.id.collapsingToolbar)
+    CollapsingToolbarLayout collapsingToolbar;
     private ShotEntity shotEntity;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
+            getWindow().getDecorView().setSystemUiVisibility(
+                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
+        }
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
         ButterKnife.bind(this);
@@ -40,7 +47,6 @@ public class ShotActivity extends AppCompatActivity {
     @Override
     protected void onResume() {
         final String title = shotEntity.getTitle();
-        custTitle.setText(title);
         if (!TextUtils.isEmpty(shotEntity.getImages().getHidpi())) {
             Picasso.with(this).load(shotEntity.getImages().getHidpi()).into(imgContent);
         }
