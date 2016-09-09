fbf1981c312a1fe0a9cd71b3d52b4c30e569a3fa
Tneciv <tnecivl@gmail.com>
2016-09-09 18:00:17 +0800

    retrofit cache support

--- app/src/main/java/com/tneciv/blueprint/module/LauncherActivity.java
+++ app/src/main/java/com/tneciv/blueprint/module/LauncherActivity.java
@@ -1,18 +1,33 @@
 package com.tneciv.blueprint.module;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Environment;
+import android.support.annotation.NonNull;
 
 import com.tneciv.blueprint.module.main.MainActivity;
+import com.tneciv.blueprint.retrofit.ApiServiceFactory;
 
 public class LauncherActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        ApiServiceFactory.cachePath = getDiskCachePath(this);
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
         finish();
     }
+
+    @NonNull
+    private String getDiskCachePath(Context context) {
+        if (Environment.MEDIA_MOUNTED.equals(Environment.getExternalStorageState()) || !Environment.isExternalStorageRemovable()) {
+            return context.getExternalCacheDir().getPath();
+        } else {
+            return context.getCacheDir().getPath();
+        }
+    }
+
 }
--- app/src/main/java/com/tneciv/blueprint/retrofit/ApiServiceFactory.java
+++ app/src/main/java/com/tneciv/blueprint/retrofit/ApiServiceFactory.java
@@ -1,10 +1,17 @@
 package com.tneciv.blueprint.retrofit;
 
+import android.text.TextUtils;
+import android.util.Log;
+
 import com.tneciv.blueprint.BuildConfig;
 
+import java.io.File;
+
+import okhttp3.Cache;
 import okhttp3.Interceptor;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
+import okhttp3.Response;
 import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
 import retrofit2.adapter.rxjava.RxJavaCallAdapterFactory;
@@ -20,6 +27,7 @@ public class ApiServiceFactory {
     private static final String BASE_API_URL = "https://api.dribbble.com/v1/";
     private static final String TOKEN = "Bearer 2074bbbaf8bae6866417a528f69780929ab6c61732473096719ef4cf210ea3be";
     private static volatile Retrofit defaultInstance;
+    public static String cachePath;
 
     private ApiServiceFactory() throws InstantiationException {
         throw new InstantiationException("This class is not for instantiation");
@@ -52,10 +60,34 @@ public class ApiServiceFactory {
                                 .build();
                         return chain.proceed(authorised);
                     };
-
                     builder.addInterceptor(mTokenInterceptor);
 
-                    OkHttpClient okHttpClient = builder.build();
+                    /**
+                     * Okhttp 缓存拦截器
+                     */
+                    Interceptor cacheInterceptor = chain -> {
+                        Request request = chain.request();
+                        if (BuildConfig.DEBUG) Log.i("ApiServiceFactory", "request=" + request);
+                        Response response = chain.proceed(request);
+                        if (BuildConfig.DEBUG) Log.i("Cache", "response=" + response);
+
+                        String cacheControl = request.cacheControl().toString();
+                        if (TextUtils.isEmpty(cacheControl)) {
+                            cacheControl = "public, max-age=60";
+                        }
+                        return response.newBuilder()
+                                .header("Cache-Control", cacheControl)
+                                .removeHeader("Pragma")
+                                .build();
+                    };
+
+                    // Cache Path
+                    File httpCacheDirectory = new File(cachePath, "responses");
+                    // Cache Size
+                    Cache cache = new Cache(httpCacheDirectory, 20 * 1024 * 1024);
+                    builder.addNetworkInterceptor(cacheInterceptor);
+
+                    OkHttpClient okHttpClient = builder.cache(cache).build();
                     defaultInstance = new Retrofit.Builder()
                             .baseUrl(BASE_API_URL)
                             .addConverterFactory(GsonConverterFactory.create())
