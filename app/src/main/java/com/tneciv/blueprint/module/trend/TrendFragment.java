6d0d23c242a8378da163c7f563c554492ea660ef
Tneciv <tnecivl@gmail.com>
2016-09-22 15:58:16 +0800

    add default animator

--- app/src/main/java/com/tneciv/blueprint/module/trend/TrendFragment.java
+++ app/src/main/java/com/tneciv/blueprint/module/trend/TrendFragment.java
@@ -5,6 +5,7 @@ import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.RecyclerView;
 
 import com.tneciv.blueprint.base.BaseListFragment;
@@ -56,6 +57,7 @@ public abstract class TrendFragment extends BaseListFragment implements TrendCon
         recyclerAdapter = new TrendRecyclerAdapter(setViewType(), list);
         recyclerAdapter.addPaginationListener(this);
         mRecyclerView.setAdapter(recyclerAdapter);
+        mRecyclerView.setItemAnimator(new DefaultItemAnimator());
         mRecyclerView.setLayoutManager(setLayoutManager(getActivity()));
     }
 
